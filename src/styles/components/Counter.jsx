ce5e9f372a39251c8e3ce04eeb115287428dbb7b
rodrigoDev6 <rodrigolima.alves21@gmail.com>
2022-01-30 16:42:42 -0300

    webpack fast refresh

--- src/styles/components/Counter.jsx
+++ src/styles/components/Counter.jsx
@@ -5,21 +5,19 @@ export function Counter(){
   const [counter, setCounter] = useState(0);
   
   function increment(){
-    
     setCounter(counter + 1)
-    
-    //console.log('incrementing')
+
   }
 
-  return (
-    <div>
-      <h1>Counter basic in react</h1>
-      <h2>{counter}</h2>
+    return (
+      <div>
+        <h1>Counter</h1>
+        <h2>{counter}</h2>
 
-      <button type="button" onClick={increment}>
-        Increment
-      </button>
+        <button className="btn-increment" type="button" onClick={increment}>
+          Increment +1
+        </button>
 
-    </div>
-  )
+      </div>
+    )
 }
